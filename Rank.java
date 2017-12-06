8af1dd26bad0e8b4d58e899ad825a19683c42ad2
Tiger Cross <tc2017@ic.ac.uk>
2017-12-06 01:08:50 +0000

    basic skeleton

---
+++ Rank.java
@@ -0,0 +1,30 @@
+public enum Rank {
+
+  ACE("A"),
+  TWO("2"),
+  THREE("3"),
+  FOUR("4"),
+  FIVE("5"),
+  SIX("6"),
+  SEVEN("7"),
+  EIGHT("8"),
+  NINE("9"),
+  TEN("10"),
+  JACK("J"),
+  QUEEN("Q"),
+  KING("K");
+
+  private final String rank;
+
+  Rank(String rank) {
+    this.rank = rank;
+  }
+
+  public String toString() {
+    return rank;
+  }
+
+  public boolean equals(){}
+
+  public boolean lessThan(){}
+}
